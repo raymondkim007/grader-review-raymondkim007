45ef052a6c3f97f2aad20d9df0b605d5b924f0cf
raymondkim007 <130116251+raymondkim007@users.noreply.github.com>
2023-06-08 20:25:03 -0700

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -24,7 +24,7 @@ cp $student_file TestListExamples.java grading-area/
 
 # compile Java files
 cd grading-area
-javac -cp .:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar *.java
+javac -cp ".;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar" *.java
 if [[ $? -eq 1 ]]
 then 
     echo "ERROR: Java file cannot be compiled. Exit code 1"
@@ -33,7 +33,7 @@ fi
 
 # run JUnit tests
 echo "Running JUnit tests..."
-java -cp .:../lib/junit-4.13.2.jar:../lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore TestListExamples > grade-results.txt
+java -cp ".;../lib/junit-4.13.2.jar;../lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples > grade-results.txt
 grep "Tests run" grade-results.txt > fail-results.txt
 grep "OK" grade-results.txt > succ-results.txt
 
